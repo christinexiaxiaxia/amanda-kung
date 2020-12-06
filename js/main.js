a08a0fbf62d263256a7d1c3e281c32b6c39cc51c
Christine <christine-xia@hotmail.com>
2020-12-06 18:46:03 -0500

    Upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// OPEN/COLLAPSE MENU
+
+// $('.menu-link.works.main').click(function(){
+// 	$('.menu-link.works.sub').toggle();
+// })
+
+// $('.menu-link.photos.main').click(function(){
+// 	$('.menu-link.photos.sub').toggle();
+// })
