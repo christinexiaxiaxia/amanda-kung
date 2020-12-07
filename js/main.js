75fef9de1689236fd92db38e9a5fa12d6c349227
Christine <christine-xia@hotmail.com>
2020-12-06 19:44:27 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -7,3 +7,185 @@
 // $('.menu-link.photos.main').click(function(){
 // 	$('.menu-link.photos.sub').toggle();
 // })
+
+
+// HOVER OVER CONTENT TO HIGHLIGHT MENU LINK
+
+$('.content.one').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.one').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.two').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.two').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.three').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.three').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.four').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.four').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.five').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.five').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.six').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.six').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.seven').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.seven').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.eight').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.eight').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.nine').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.nine').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.content.ten').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.ten').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+
+
+
+
+
+// MENU LINK HOVER
+
+$('.menu-link.works.one').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.one').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.two').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.two').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.three').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.three').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.four').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.four').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.five').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.five').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.six').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.six').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.seven').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.seven').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.eight').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.eight').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.nine').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.nine').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+$('.menu-link.works.ten').hover(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)'})
+		$('.menu-link.works.ten').css({'color':'black'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
+
+
+
+// MENU LINK CLICK
+
+$('.menu-link.works.one').click(
+	function(){
+		$('.menu-link.works').css({'color':'rgb(170,170,170)','font-style':'normal'})
+		$('.menu-link.works.one').css({'color':'black','font-style':'italic'})
+		$('.menu-link.works.main').css({'color':'black'})
+	}
+)
